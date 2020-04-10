dcfe9a0cc72138f00104214c5faae602011c97f1
a_m_dev <a.mirzaei.dev@gmail.com>
2020-04-10 10:49:07 +0430

    material practiceing

--- src/js/Components/Material_ui/Material.jsx
+++ src/js/Components/Material_ui/Material.jsx
@@ -1,13 +1,96 @@
-import React from "react";
-import Button from "@material-ui/core/Button";
+import "date-fns";
+import React, { useState, useReducer } from "react";
+import clsx from "clsx";
+import {
+  TextField,
+  Paper,
+  Grid,
+  BottomNavigation,
+  BottomNavigationAction,
+  Drawer,
+  SwipeableDrawer,
+  Button,
+  List,
+  Divider,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@material-ui/core";
+import { Inbox, Mail } from "@material-ui/icons";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // width: 900,
+    padding: theme.spacing(3),
+    // margin: theme.spacing(2, 0),
+  },
+  list: { width: 200 },
+  fullList: { width: "auto" },
+}));
 
 export default function Material_UI(props) {
+  const classes = useStyles();
+  const [state, setState] = useState({
+    top: false,
+    bottom: false,
+    left: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (e) => {
+    if (e.type === "keydown" && (e.key === "Tab" || e.key === "Shift")) return;
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes, list, {
+        [classes.fullList]: anchor === "top" || anchor === "bottom",
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <Inbox /> : <Mail />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <Inbox /> : <Mail />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
   return (
-    <article className="container box box--white">
-      <h1>hi there</h1>
-      <Button variant="contained" color="primary">
-        Hi there
-      </Button>
-    </article>
+    <Paper className={classes.root}>
+      {["left", "right", "top", "bottom"].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </Paper>
   );
 }
